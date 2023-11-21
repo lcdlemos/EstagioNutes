f3d4322c300f49361d3fd059c56c89833c9970eb
Luiz Carlos <luizlemos02@gmail.com>
2023-11-21 00:37:05 -0300

    criando modelo automatico

---
+++ Slides/funcoes.py
@@ -0,0 +1,44 @@
+def contar_slides(nome_arquivo):
+    with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
+        texto = arquivo.read()
+        num_slides = texto.count("Slide")
+    return num_slides
+
+def ler_arquivo(nome_arquivo):
+    with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
+        texto = arquivo.readlines()
+    return texto
+
+def criar_capa(auto_apresentacao, texto):
+    modelo_slide = auto_apresentacao.slide_layouts[0]
+    slide = auto_apresentacao.slides.add_slide(modelo_slide)
+    titulo = slide.shapes.title
+    subtitulo = slide.placeholders[1]
+    titulo.text = texto[1].lstrip("- Título: ").rstrip("\n")
+    subtitulo.text = texto[2].strip("\n")
+    return slide
+
+def coletar_titulos(texto):
+    titulos = []
+    for linha in texto:
+        if "Título" in linha:
+            titulos.append(linha)
+    return titulos
+
+def coletar_topicos(texto):
+    topicos = []
+    for topico in texto:
+        if "- " in topico and "Título" not in topico and "Autor:" not in topico or "\n" == topico:
+            topicos.append(topico)
+    return topicos
+
+def adicionar_slides(auto_apresentacao, texto, titulos, topicos, num_slides):
+    slides = []
+    for i in range(num_slides - 1):
+        modelo_slide = auto_apresentacao.slide_layouts[1]
+        slide = auto_apresentacao.slides.add_slide(modelo_slide)
+        titulo = slide.shapes.title
+        titulo.text = titulos[i+1].lstrip("- Título: ").rstrip("\n")
+        
+        slides.append(slide)
+    return slides
